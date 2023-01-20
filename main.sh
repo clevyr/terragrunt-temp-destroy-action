33053ee124c808d5448ccd3b2c19fe46a8d45cc6
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2023-01-20 15:14:26 -0700

    Swap to new GKE auth plugin

--- main.sh
+++ main.sh
@@ -57,6 +57,11 @@ docker_repo="${REPO_URL:-us.gcr.io/$project_id}"
 echo "$GCLOUD_KEY_FILE" > /tmp/serviceAccount.json
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/serviceAccount.json
 
+_log Install GKE auth plugin
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+
 _log Select Kubernetes cluster
 gcloud container clusters get-credentials  \
     "$cluster_name" \
